d54b8ff8e17495fe47acb137b765d734d9eaa8bb
aseemyash <91648278+aseemyash@users.noreply.github.com>
2022-05-14 23:20:13 +0530

    Add files via upload

---
+++ naya.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "ASEEM YASH APKA YAH SHASTRA APKE LIYE SADEV TAIYAR HAI"
+sleep 0.1
+read -p "bhai ip address bta abhi gand marta hu iski:" ip
+sleep 1
+echo "ip ka pura janam kundali nikal raha hu"
+whois $ip
+echo "ip ka kya firewall check krna raha hu"
+wafw00f $ip
+echo "bhai backend ka services check krlete ha"
+whatweb $ip
+echo "SAALE KA LOCATION PATA KRTE HAI"
+curl https://api.hackertarget.com/geoip/?q=$ip
+echo "nmap bhi kre ke pura ghar me khidke se dekh lete hai"
+nmap -Pn -sV  $ip
+echo "nikto krna hai kya"
+read nikto
+if [ $nikto ]; then
+nikto --url $ip
+else 
+    echo "kam hogaya bhai"
+fi
